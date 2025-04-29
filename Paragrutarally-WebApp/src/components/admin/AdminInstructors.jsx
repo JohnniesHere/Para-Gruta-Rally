f3858bf900f521f8cd6b2ad3ef4a597462c6f509
Hagi Debby <hagidebby@gmail.com>
2025-04-29 15:17:04 +0300

    exported detail page for instructors
    removed the duped version from adminInstructors into its own file

--- Paragrutarally-WebApp/src/components/admin/AdminInstructors.jsx
+++ Paragrutarally-WebApp/src/components/admin/AdminInstructors.jsx
@@ -37,28 +37,3 @@ const AdminInstructors = () => {
 };
 
 export default AdminInstructors;
-
-// src/pages/admin/InstructorDetailPage.jsx
-import React from 'react';
-import { useParams, Link } from 'react-router-dom';
-import InstructorDetail from '../../components/instructors/InstructorDetail';
-
-const InstructorDetailPage = () => {
-    const { instructorId } = useParams();
-
-    return (
-        <div className="instructor-detail-page">
-            <div className="page-header">
-                <div className="breadcrumbs">
-                    <Link to="/admin/instructors">Instructors</Link> / Instructor Details
-                </div>
-            </div>
-
-            <div className="card">
-                <InstructorDetail instructorId={instructorId} />
-            </div>
-        </div>
-    );
-};
-
-export default InstructorDetailPage;
